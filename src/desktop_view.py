b700d7702c656caf932107c422369780359fc458
andrvat <andreivalitov2001@gmail.com>
2022-04-27 00:37:51 +0700

    add screen manager

--- src/desktop_view.py
+++ src/desktop_view.py
@@ -3,16 +3,16 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
-from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.textinput import TextInput
 
 
-class DesktopApp(App):
+class InputScreen(Screen):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        super(InputScreen, self).__init__(**kwargs)
+
         self.color_code = '#79FF8F'
         self.size_hint = (1, 0.7)
-        self.window = None
         self.question = Label(
             text="Your phrase of interest?",
             font_size=24,
@@ -30,23 +30,30 @@ class DesktopApp(App):
             background_color=self.color_code,
         )
 
-    def build(self):
-        self.window = GridLayout()
-        self.window.cols = 1
-        self.window.size_hint = (0.4, 0.3)
-        self.window.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
+        self.main = GridLayout()
+        self.main.cols = 1
+        self.main.size_hint = (0.5, 0.4)
+        self.main.pos_hint = {'center_x': 0.5, 'center_y': 0.8}
 
-        self.window.add_widget(self.question)
-
-        self.window.add_widget(self.input)
+        self.main.add_widget(self.question)
+        self.main.add_widget(self.input)
+        self.main.add_widget(self.submit)
 
         self.submit.bind(on_press=self.callback)
-        self.window.add_widget(self.submit)
 
-        return self.window
+        self.add_widget(self.main)
 
     def callback(self, instance):
         self.question.text = "Hello " + self.input.text + "!"
+
+
+class DesktopApp(App):
+    def build(self):
+        manager = ScreenManager()
+        input_screen = InputScreen(name='input')
+
+        manager.add_widget(input_screen)
+        return manager
 
 
 DesktopApp().run()
